8c90df07b46a4bb435a6648efeb624f728a524a1
walkerNegrão <walker.esteves.negrao@gmail.com>
2024-02-14 07:44:42 -0300

    Repositório criado

---
+++ src/main/java/com/walker/microsservicerabbitmqmat/MicrosserviceRabbitmqMatApplication.java
@@ -0,0 +1,13 @@
+package com.walker.microsservicerabbitmqmat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicrosserviceRabbitmqMatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicrosserviceRabbitmqMatApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/walker/microsservicerabbitmqmat/MicrosserviceRabbitmqMatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.walker.microsservicerabbitmqmat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicrosserviceRabbitmqMatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
